efdeca8a65c304e886421fb8b8a9942574153e63
Attila Szabo <aszabo@vrvis.at>
2016-03-01 20:04:41 +0100

    faster terrain quadtree traversal using mutable list

--- src/Tg-Fsharp/Terrain.fs
+++ src/Tg-Fsharp/Terrain.fs
@@ -67,44 +67,46 @@ module Terrain =
 //
 //        bottomleft - bottommiddle -- bottomright
 
-        let rec continueForks ( corners : Fork<FloorPoint> ) ( level : int ) : Floor =
-            match level with
-                | 0 -> { Corners = corners; After = None}
-                | _ -> 
-                    let nextlevel = level-1
-
-                    let topleft = corners.TopLeft
-                    let topright = corners.TopRight
-                    let bottomleft = corners.BotLeft
-                    let bottomright = corners.BotRight
-                    
-                    let topmiddle = topleft.HalfwayTowards topright
-                    let leftmiddle = topleft.HalfwayTowards bottomleft
-                    let bottommiddle = bottomleft.HalfwayTowards bottomright
-                    let rightmiddle = topright.HalfwayTowards bottomright
-
-                    //center is average of all four corners.
-                    let oldCenterTLBR = topleft.HalfwayTowards bottomright
-                    let oldCenterTRBL = topright.HalfwayTowards bottomleft
-                    let oldCenter = oldCenterTLBR.HalfwayTowards oldCenterTRBL
-
-                    //center has a random height. That's why its a fractal terrain.
-                    //let random = Rng.random -0.1 0.1
-                    let random = Rng.gaussian 0.0 0.05
-                    let center = { oldCenter with Height = oldCenter.Height + random }
-
-                    {
-                        Corners = corners
-                        After   = 
-                            Some {      
-                                TopLeft = continueForks { TopLeft = topleft; TopRight = topmiddle; BotLeft = leftmiddle; BotRight = center } nextlevel
-                                TopRight= continueForks { TopLeft = topmiddle; TopRight = topright; BotLeft = center; BotRight = rightmiddle } nextlevel
-                                BotLeft = continueForks { TopLeft = leftmiddle; TopRight = center; BotLeft = bottomleft; BotRight = bottommiddle } nextlevel
-                                BotRight= continueForks { TopLeft = center; TopRight = rightmiddle; BotLeft = bottommiddle; BotRight = bottomright } nextlevel
-                            }
-                    }
-
         let floor ( levels : int ) =
+            let rec continueForks ( corners : Fork<FloorPoint> ) ( level : int ) : Floor =
+                match level with
+                    | 0 -> { Corners = corners; After = None}
+                    | _ -> 
+                        let nextlevel = level-1
+
+                        let topleft = corners.TopLeft
+                        let topright = corners.TopRight
+                        let bottomleft = corners.BotLeft
+                        let bottomright = corners.BotRight
+                    
+                        let topmiddle = topleft.HalfwayTowards topright
+                        let leftmiddle = topleft.HalfwayTowards bottomleft
+                        let bottommiddle = bottomleft.HalfwayTowards bottomright
+                        let rightmiddle = topright.HalfwayTowards bottomright
+
+                        //center is average of all four corners.
+                        let oldCenterTLBR = topleft.HalfwayTowards bottomright
+                        let oldCenterTRBL = topright.HalfwayTowards bottomleft
+                        let oldCenter = oldCenterTLBR.HalfwayTowards oldCenterTRBL
+
+                        //center has a random height. That's why its a fractal terrain.
+
+                        let weight = float level / float levels
+                        //let random = Rng.random -0.1 0.1
+                        let random = weight * Rng.gaussian 0.0 0.05
+                        let center = { oldCenter with Height = oldCenter.Height + random }
+
+                        {
+                            Corners = corners
+                            After   = 
+                                Some {      
+                                    TopLeft = continueForks { TopLeft = topleft; TopRight = topmiddle; BotLeft = leftmiddle; BotRight = center } nextlevel
+                                    TopRight= continueForks { TopLeft = topmiddle; TopRight = topright; BotLeft = center; BotRight = rightmiddle } nextlevel
+                                    BotLeft = continueForks { TopLeft = leftmiddle; TopRight = center; BotLeft = bottomleft; BotRight = bottommiddle } nextlevel
+                                    BotRight= continueForks { TopLeft = center; TopRight = rightmiddle; BotLeft = bottommiddle; BotRight = bottomright } nextlevel
+                                }
+                        }
+            
             Report.BeginTimed("Terrain generation for {0} lvls", levels)
             let start = 
                 {
--- src/Tg-Fsharp/Visuals.fs
+++ src/Tg-Fsharp/Visuals.fs
@@ -3,6 +3,7 @@
 module Visuals =
     
     open System
+    open System.Collections.Generic
 
     open Aardvark.Base
     open Aardvark.Base.Incremental
@@ -76,7 +77,8 @@ module Visuals =
                     
             let setRender (sg : ISg) = 
                 rc.RenderTask <- 
-                    oglapp.Runtime.CompileRender(rc.FramebufferSignature, BackendConfiguration.ManagedOptimized, sg) |> DefaultOverlays.withStatistics
+                    oglapp.Runtime.CompileRender(rc.FramebufferSignature, BackendConfiguration.ManagedOptimized, sg) 
+//                        |> DefaultOverlays.withStatistics
 
         //this is a RenderControl that depends on one Floor as its content
         let ofFloor ( floor : IMod<Terrain.Floor> ) ( scale : IMod<float> ) =
@@ -90,43 +92,41 @@ module Visuals =
 
                 v0.Cross v1 |> oriented, v0.Cross v2 |> oriented
 
-            let rec floorGeometry ( corners : Fork<FloorPoint> ) ( cw : Orientation ) ( after : Option<Fork<Floor>> ) =
+            let rec floorGeometry ( corners : Fork<FloorPoint> ) ( cw : Orientation ) ( after : Option<Fork<Floor>> )
+                (pos:List<V3d>) (norm:List<V3d>) (col:List<C4b>) =
                 match after with
                     //there are no successors, the result is a Sg made of the current geometry (list of single element)
                     | None ->                         
-                        let (positions, colors, normals) = 
-                            let topleftpoint = corners.TopLeft      |> vfp
-                            let toprightpoint = corners.TopRight    |> vfp
-                            let bottomleftpoint = corners.BotLeft   |> vfp
-                            let bottomrightpoint = corners.BotRight |> vfp
+                        let topleftpoint = corners.TopLeft      |> vfp
+                        let toprightpoint = corners.TopRight    |> vfp
+                        let bottomleftpoint = corners.BotLeft   |> vfp
+                        let bottomrightpoint = corners.BotRight |> vfp
 
-                            //orientation determines where the diagonal is (has to point toward the middle).
-                            //all vertices appear twice since they have different normals.
-                            match cw with
-                                | Clockwise ->
-                                    [| topleftpoint; toprightpoint; bottomrightpoint; topleftpoint; bottomrightpoint; bottomleftpoint |],
-                                    [| C4b.White; C4b.White; C4b.White; C4b.White; C4b.White; C4b.White |],
+                        //orientation determines where the diagonal is (has to point toward the middle).
+                        //all vertices appear twice since they have different normals.
+                        match cw with
+                            | Clockwise ->
+                                pos.Add topleftpoint; pos.Add toprightpoint; pos.Add bottomrightpoint; pos.Add topleftpoint; pos.Add bottomrightpoint; pos.Add bottomleftpoint
+                                col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White
 
-                                    let v0 = (bottomrightpoint - topleftpoint).Normalized
-                                    let v1 = (toprightpoint    - topleftpoint).Normalized
-                                    let v2 = (bottomleftpoint  - topleftpoint).Normalized
+                                let v0 = (bottomrightpoint - topleftpoint).Normalized
+                                let v1 = (toprightpoint    - topleftpoint).Normalized
+                                let v2 = (bottomleftpoint  - topleftpoint).Normalized
 
-                                    let (n012,n023) = norms v0 v1 v2
+                                let (n012,n023) = norms v0 v1 v2
 
-                                    [| n012; n012; n012; n023; n023; n023 |]
-                                | Counterclockwise ->
-                                    [| toprightpoint; bottomrightpoint; bottomleftpoint; toprightpoint; bottomleftpoint; topleftpoint |],
-                                    [| C4b.White; C4b.White; C4b.White; C4b.White; C4b.White; C4b.White |],
+                                norm.Add n012; norm.Add n012; norm.Add n012; norm.Add n023; norm.Add n023; norm.Add n023
+                            | Counterclockwise ->
+                                pos.Add toprightpoint; pos.Add  bottomrightpoint; pos.Add  bottomleftpoint; pos.Add  toprightpoint; pos.Add  bottomleftpoint; pos.Add  topleftpoint
+                                col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White; col.Add C4b.White
 
-                                    let v0 = (bottomleftpoint  - toprightpoint).Normalized
-                                    let v1 = (bottomrightpoint - toprightpoint).Normalized
-                                    let v2 = (topleftpoint     - toprightpoint).Normalized
+                                let v0 = (bottomleftpoint  - toprightpoint).Normalized
+                                let v1 = (bottomrightpoint - toprightpoint).Normalized
+                                let v2 = (topleftpoint     - toprightpoint).Normalized
 
-                                    let (n012,n023) = norms v0 v1 v2
+                                let (n012,n023) = norms v0 v1 v2
 
-                                    [| n012; n012; n012; n023; n023; n023 |]
-
-                        positions, normals, colors
+                                norm.Add n012; norm.Add n012; norm.Add n012; norm.Add n023; norm.Add n023; norm.Add n023
 
                     //there are successors, our result is the list of their results
                     | Some after ->                                     
@@ -135,31 +135,42 @@ module Visuals =
                         let BL = after.BotLeft
                         let BR = after.BotRight
                         
-                        let (tlp, tln, tlc) = floorGeometry TL.Corners Clockwise        TL.After
-                        let (trp, trn, trc) = floorGeometry TR.Corners Counterclockwise TR.After
-                        let (blp, bln, blc) = floorGeometry BL.Corners Counterclockwise BL.After
-                        let (brp, brn, brc) = floorGeometry BR.Corners Clockwise        BR.After
-                        [tlp; trp; blp; brp] |> Array.concat,
-                        [tln; trn; bln; brn] |> Array.concat,
-                        [tlc; trc; blc; brc] |> Array.concat        //continuation monad
+                        floorGeometry TL.Corners Clockwise        TL.After pos norm col
+                        floorGeometry TR.Corners Counterclockwise TR.After pos norm col
+                        floorGeometry BL.Corners Counterclockwise BL.After pos norm col
+                        floorGeometry BR.Corners Clockwise        BR.After pos norm col
 
                         
             let floorISg ( floor : Floor ) =
                 Report.BeginTimed("Creating Visualization ")
-                let infos = floorGeometry floor.Corners Clockwise floor.After
+
+                //using a mutable list here, which is filled by the traversal function through mutation. 
+                //with this list additions are very cheap, and we have a lot of those. compared to the default F# list,
+                //this is about twice as fast and needs about 70% memory. need to think about a better functional list
+                //here (concat lists, continuation lists ....)
+                let poslist = List<V3d>()
+                let normlist = List<V3d>()
+                let collist = List<C4b>()
+
+                floorGeometry floor.Corners Clockwise floor.After
+                                poslist normlist collist
+
+                let pos = poslist.ToArray()
+                let norm = normlist.ToArray()
+                let col = collist.ToArray()
+
                 let res =
                     let (indexedAttributes, indices) = 
-                        let (pos, norm, col) = infos
                         [
                             DefaultSemantic.Positions, pos :> Array
                             DefaultSemantic.Normals, norm :> Array
                             DefaultSemantic.Colors, col :> Array
                         ] |> SymDict.ofList,
-                        let vc = ((pos |> Array.length) - 1)
+                        let vc = pos |> Array.length
                         let indices = Array.init vc id
 
-                        Report.Line("Number vertices {0}", vc)
-                        Report.Line("Estimated size attributes: {0} MB", (float vc * 6.0 * 4.0 * 3.0)/1024.0/1024.0)
+                        //patch count * vertices per patch * size of float * attributes of vertex * copies of each attribute in memory
+                        Report.Line("{0} vertices, estimated memory: {1:0.000} MB", vc, (float vc * 6.0 * 4.0 * 3.0 * 3.0)/1024.0/1024.0)
 
                         indices
                         
